d584df501efd1fb675facb21540916bc81325f52
andinoxis <andinoxis@minet.net>
2026-04-25 03:07:16 +0200

    fix: frontend

--- frontend/src/pages/AdminPage.tsx
+++ frontend/src/pages/AdminPage.tsx
@@ -282,7 +282,7 @@ export default function AdminPage() {
     return (
       <div className="flex flex-col gap-3 h-full">
         <div className="shrink-0 border-b border-neutral-200 dark:border-neutral-700 pb-2">{tabBar}</div>
-        <SecurityTab />
+        <SecurityTab userLookup={userLookup} />
       </div>
     )
   }
